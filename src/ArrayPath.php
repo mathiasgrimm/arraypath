ef5b097a0a6e74da5994145b37ce7e55cca772f4
Rafael Dohms <rdohms@gmail.com>
2015-03-20 11:04:16 +0100

    Update ArrayPath.php
    Fixed Namespace to use StudlyCaps

--- src/ArrayPath.php
+++ src/ArrayPath.php
@@ -6,7 +6,7 @@
  * @copyright Copyright (c) 2014 Mathias Grimm. (http://github.com/mathiasgrimm)
  * @license   https://github.com/mathiasgrimm/arraypath/blob/master/LICENSE.txt Mathias Grimm License
  */
-namespace mathiasgrimm\arraypath;
+namespace Mathiasgrimm\Arraypath;
 
 class ArrayPath
 {
