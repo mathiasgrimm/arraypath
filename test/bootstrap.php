0fddb6014e7e50f6406f39d50075a327a76759fe
Mathias Grimm <mathiasgrimm@gmail.com>
2015-03-22 17:26:20 +0000

    removing x dependency

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,23 +3,3 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 require '../vendor/autoload.php';
-
-new mathiasgrimm\X\X();
-
-// spl_autoload_register(function($sClass){
-//     if (strpos($sClass, 'mathiasgrimm\\htmlwrappers\\') === 0) {
-//         $sClass = str_replace('mathiasgrimm\\htmlwrappers\\', '', $sClass);
-//         $sPath  = "../src/{$sClass}.php";
-
-//         require $sPath;  
-//     } 
-// });
-
-// function __autoload($sClass)
-// {
-//     xd($sClass);
-//     require "../src/{$sClass}.php"; 
-// }
-
-
-
